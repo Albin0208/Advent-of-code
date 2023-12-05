f0e2cf2e424a6c481996260ec51aa85ea6332f8e
Albin Dahlén <albin@dahlen.ws>
2023-12-05 17:37:12 +0100

    Day 5 Init

---
+++ 2023/Day5/Day5.cpp
@@ -0,0 +1,15 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+
+int main()
+{
+    std::ifstream file("input.txt");
+
+    std::string line;
+
+    while (std::getline(file, line))
+    {
+	    std::cout << line << std::endl;
+	}
+}
