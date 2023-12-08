46c96c40b5c772dee30adee6227b38b319b17c00
Albin Dahlén <albin@dahlen.ws>
2023-12-08 12:14:31 +0100

    Day6 part 2

--- 2023/Day6/Day6.cpp
+++ 2023/Day6/Day6.cpp
@@ -13,26 +13,31 @@ int main()
 	std::istringstream iss(buffer.str());
 	std::string line;
 
-	std::vector<int> in_table;
+	std::vector<long long> in_table;
 	iss >> line; // Read out the Time:
+	std::string s;
 	while (iss >> line)
 	{
 		// No digit continue to next
 		if (!std::isdigit(line[0]))
+		{
+			in_table.push_back(std::stoll(s));
+			s = "";
 			continue;
-
-		in_table.push_back(std::stoi(line));
+		}
+		s += line;
 	}
 
+	in_table.push_back(std::stoll(s));
 	// in_table / 2 give time from 0 to mid and distance from mid to end
 
 	int half_table_i = in_table.size() / 2 ;
 
-	int sum{1};
+	long long sum{1};
 	for (int i{}; i < half_table_i; ++i)
 	{
-		int const time = in_table[i];
-		int beat_num{};
+		long long const time = in_table[i];
+		long long beat_num{};
 		for (int j{1}; j < time; ++j) // start at 1 since 0 gives 0 distance no need to check last since it will also move 0
 			if ((time - j) * j > in_table[half_table_i + i])
 				beat_num++;
